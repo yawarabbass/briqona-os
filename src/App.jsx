a9567d6fbbba1ed621c93ebb769158979e8fba38
yawarabbass <35185083+yawarabbass@users.noreply.github.com>
2026-07-28 17:09:40 +0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>BRIQONA OS</h1>
+      <p>Intelligent Business Operating System</p>
+    </div>
+  );
+}
+
+export default App;
