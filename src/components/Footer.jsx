1288b629f128e9047081be3b5198f22ab7827065
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-07-30 09:43:07 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,15 @@
+import React from "react";
 
+function Footer() {
+  return (
+    <footer className="site-footer">
+      <div className="container footer-bottom">
+        <span>© 2026 Briqona OS. All rights reserved.</span>
+
+        <span>Designed & Developed with care by Yawar Abbas Bhatti</span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
