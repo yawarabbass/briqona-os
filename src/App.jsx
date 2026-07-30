049e9d9770c00a8cbf605201f0fa935c6f2c09db
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-07-30 09:27:36 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
 import React from "react";
-import Home from "./pages/home.jsx";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MobileMenu from "./components/MobileMenu";
+
+import Home from "./pages/home";
+import Platform from "./pages/Platform";
+import Pricing from "./pages/Pricing";
 
 function App() {
-  return <Home />;
+  const path = window.location.pathname;
+
+  const routes = {
+    "/": <Home />,
+    "/index.html": <Home />,
+    "/platform": <Platform />,
+    "/pricing": <Pricing />,
+  };
+
+  const page = routes[path] || <Home />;
+
+  return (
+    <>
+      <Header />
+
+      <MobileMenu />
+
+      <main id="main-content">
+        {page}
+      </main>
+
+      <Footer />
+    </>
+  );
 }
 
 export default App;
