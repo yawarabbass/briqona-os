701f6b509f04b643d08aca9804a3ee0a2f328e37
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-07-30 09:54:56 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import MobileMenu from "./components/MobileMenu";
 
 import Home from "./pages/home";
 import Platform from "./pages/Platform";
@@ -23,8 +22,6 @@ function App() {
   return (
     <>
       <Header />
-
-      <MobileMenu />
 
       <main id="main-content">
         {page}
