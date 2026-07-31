1fa646798ed605bf0364ef5ed754322af0dcbecd
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-07-31 22:01:23 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import WhatsApp from "./components/WhatsApp/WhatsApp";
 
 import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="app">
-
       <Header />
 
       <Home />
 
       <Footer />
 
+      <WhatsApp />
     </div>
   );
 }
