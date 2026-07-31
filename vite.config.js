a4eeb9e1d0b8c2c1737f1cc311edce1185e1b28c
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-07-31 23:40:55 +0500

    Update vite.config.js
    fix github pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/briqona-os/",
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-  },
+
+  base: "/briqona-os/",
+
   build: {
     outDir: "dist",
     sourcemap: false,
