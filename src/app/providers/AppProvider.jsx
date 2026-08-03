cb3d982992c24e86f384c56b5d2d39327f1353f1
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-04 02:52:24 +0500

    Create AppProvider.jsx

---
+++ src/app/providers/AppProvider.jsx
@@ -0,0 +1,13 @@
+/*
+|--------------------------------------------------------------------------
+| BRIQONA OS - Application Provider
+|--------------------------------------------------------------------------
+|
+| Global application provider.
+| Future providers will be connected here.
+|
+*/
+
+export default function AppProvider({ children }) {
+  return children;
+}
