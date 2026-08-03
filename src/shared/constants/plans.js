ea9d25b8c3e50c58baa83a15cf50ed7bcd8a64c2
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-04 02:58:11 +0500

    Create plans.js

---
+++ src/shared/constants/plans.js
@@ -0,0 +1,25 @@
+/*
+|--------------------------------------------------------------------------
+| BRIQONA OS - Subscription Plans
+|--------------------------------------------------------------------------
+|
+| Central list of SaaS subscription plans.
+| Used for billing, feature unlocking and tenant provisioning.
+|
+*/
+
+const Plans = {
+  TRIAL: "trial",
+
+  STARTER: "starter",
+
+  GROWTH: "growth",
+
+  BUSINESS: "business",
+
+  ENTERPRISE: "enterprise",
+
+  CUSTOM: "custom",
+};
+
+export default Plans;
