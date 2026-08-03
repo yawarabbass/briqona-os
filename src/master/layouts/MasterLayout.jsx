3ed9047071131ed74ec19afb473b609156768f49
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-04 03:14:27 +0500

    Update MasterLayout.jsx

--- src/master/layouts/MasterLayout.jsx
+++ src/master/layouts/MasterLayout.jsx
@@ -1,7 +1,39 @@
+/*
+|--------------------------------------------------------------------------
+| BRIQONA OS
+|--------------------------------------------------------------------------
+| Master Layout
+|--------------------------------------------------------------------------
+|
+| Platform Owner Layout
+| Used by all Master Panel pages.
+|
+*/
+
 export default function MasterLayout({ children }) {
   return (
-    <>
-      {children}
-    </>
+    <div className="master-layout">
+
+      {/* Sidebar */}
+      <aside className="master-sidebar">
+
+      </aside>
+
+      {/* Main Area */}
+      <div className="master-main">
+
+        {/* Header */}
+        <header className="master-header">
+
+        </header>
+
+        {/* Page Content */}
+        <main className="master-content">
+          {children}
+        </main>
+
+      </div>
+
+    </div>
   );
 }
