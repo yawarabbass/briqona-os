8c79cb61c527ca3bf4298200fc5c075131242593
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-04 02:48:30 +0500

    Create AppConfig.js

---
+++ src/app/config/AppConfig.js
@@ -0,0 +1,25 @@
+/*
+|--------------------------------------------------------------------------
+| BRIQONA OS Application Configuration
+|--------------------------------------------------------------------------
+|
+| Central application configuration.
+| Backend URLs and environment values will be added later.
+|
+*/
+
+const AppConfig = {
+  appName: "BRIQONA OS",
+
+  version: "1.0.0",
+
+  environment: "development",
+
+  publicUrl: "/",
+
+  workspaceUrl: "/workspace",
+
+  masterUrl: "/master",
+};
+
+export default AppConfig;
