d1851725556fe0af25d81394906caf058e220a27
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-04 02:59:36 +0500

    Create Dashboard.jsx

---
+++ src/master/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import ComingSoon from "../../shared/components/ComingSoon";
+
+export default function Dashboard() {
+  return (
+    <ComingSoon
+      title="Master Dashboard"
+      description="Master Panel Dashboard will be implemented here."
+    />
+  );
+}
