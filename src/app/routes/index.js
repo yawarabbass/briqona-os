e22649a446bb668d8ef1c8840125e5c4d53a308e
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-04 02:47:09 +0500

    Create index.js

---
+++ src/app/routes/index.js
@@ -0,0 +1,21 @@
+/*
+|--------------------------------------------------------------------------
+| BRIQONA OS Routes
+|--------------------------------------------------------------------------
+|
+| This file will manage all application routes.
+| React Router configuration will be added later.
+|
+*/
+
+const routes = {
+  public: "/",
+  login: "/login",
+  register: "/register",
+  demo: "/demo",
+
+  workspace: "/workspace",
+  master: "/master",
+};
+
+export default routes;
