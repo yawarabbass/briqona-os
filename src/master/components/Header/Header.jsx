07b14449baeed3787e90022d3c5cb88cb15942e5
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-04 03:16:16 +0500

    Create Header.jsx

---
+++ src/master/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+/*
+|--------------------------------------------------------------------------
+| BRIQONA OS
+|--------------------------------------------------------------------------
+| Master Header
+|--------------------------------------------------------------------------
+*/
+
+export default function Header() {
+  return (
+    <header className="master-header">
+
+      <div className="master-header-left">
+
+      </div>
+
+      <div className="master-header-center">
+
+      </div>
+
+      <div className="master-header-right">
+
+      </div>
+
+    </header>
+  );
+}
