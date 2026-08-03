d0895dda0c5e0009064d1f90f9d2a04681223390
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-04 03:19:20 +0500

    Create Sidebar.jsx

---
+++ src/master/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,29 @@
+/*
+|--------------------------------------------------------------------------
+| BRIQONA OS
+|--------------------------------------------------------------------------
+| Master Sidebar
+|--------------------------------------------------------------------------
+*/
+
+export default function Sidebar() {
+  return (
+    <aside className="master-sidebar">
+
+      <div className="master-sidebar-logo">
+
+      </div>
+
+      <nav className="master-sidebar-nav">
+
+      </nav>
+
+      <div className="master-sidebar-footer">
+
+      </div>
+
+    </aside>
+  
+    
+  );
+}
