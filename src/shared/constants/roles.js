c369af4746cdc8b83648e8136217541f0316df09
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-04 02:53:37 +0500

    Create roles.js

---
+++ src/shared/constants/roles.js
@@ -0,0 +1,27 @@
+/*
+|--------------------------------------------------------------------------
+| BRIQONA OS - System Roles
+|--------------------------------------------------------------------------
+|
+| Central list of application roles.
+| Used across Master Panel and Workspace.
+|
+*/
+
+const Roles = {
+  MASTER_OWNER: "master_owner",
+
+  SUPER_ADMIN: "super_admin",
+
+  COMPANY_OWNER: "company_owner",
+
+  COMPANY_ADMIN: "company_admin",
+
+  MANAGER: "manager",
+
+  EMPLOYEE: "employee",
+
+  GUEST: "guest",
+};
+
+export default Roles;
