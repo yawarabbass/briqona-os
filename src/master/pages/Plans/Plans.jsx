17003fb568990c8edea01da09dfd113e6cb13547
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-04 16:18:37 +0500

    Create Plans.jsx

---
+++ src/master/pages/Plans/Plans.jsx
@@ -0,0 +1,38 @@
+import MasterLayout from "../../layouts/MasterLayout";
+import "./Plans.css";
+
+import PlanSearch from "../../components/PlanSearch/PlanSearch";
+import PlanFilters from "../../components/PlanFilters/PlanFilters";
+import PlanTable from "../../components/PlanTable/PlanTable";
+
+export default function Plans() {
+  return (
+    <MasterLayout>
+
+      <section className="master-plans">
+
+        <div className="master-page-header">
+
+          <h1>Plans</h1>
+
+          <p>
+            Manage subscription plans and pricing.
+          </p>
+
+        </div>
+
+        <div className="plans-toolbar">
+
+          <PlanSearch />
+
+          <PlanFilters />
+
+        </div>
+
+        <PlanTable />
+
+      </section>
+
+    </MasterLayout>
+  );
+}
