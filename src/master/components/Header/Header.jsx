3ea29967a7b031566cc2700f571143c7fb0ced9a
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-04 11:49:58 +0500

    Update Header.jsx

--- src/master/components/Header/Header.jsx
+++ src/master/components/Header/Header.jsx
@@ -6,12 +6,16 @@
 |--------------------------------------------------------------------------
 */
 
+import "./Header.css";
+
 export default function Header() {
   return (
     <header className="master-header">
 
       <div className="master-header-left">
-
+        <h1 className="master-page-title">
+          Dashboard
+        </h1>
       </div>
 
       <div className="master-header-center">
@@ -19,6 +23,14 @@ export default function Header() {
       </div>
 
       <div className="master-header-right">
+
+        <button className="master-header-btn">
+          Notifications
+        </button>
+
+        <button className="master-header-btn">
+          Profile
+        </button>
 
       </div>
 
