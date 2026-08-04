894ec4ac7ab802f8cb1fbafa73e9eabd6649ed17
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-04 11:44:40 +0500

    Create Dashboard.jsx

---
+++ src/master/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,19 @@
+/*
+|--------------------------------------------------------------------------
+| BRIQONA OS
+|--------------------------------------------------------------------------
+| Master Dashboard
+|--------------------------------------------------------------------------
+*/
+
+import MasterLayout from "../../layouts/MasterLayout";
+
+export default function Dashboard() {
+  return (
+    <MasterLayout>
+
+      <h1>Master Dashboard</h1>
+
+    </MasterLayout>
+  );
+}
