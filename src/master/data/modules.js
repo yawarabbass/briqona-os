b1656ea75dabaa4fde468e3e709f64809e44bf13
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-04 16:42:39 +0500

    Create modules.js

---
+++ src/master/data/modules.js
@@ -0,0 +1,32 @@
+const modules = [
+  {
+    id: 1,
+    name: "CRM",
+    industry: "Technology",
+    version: "1.0.0",
+    status: "Active",
+  },
+  {
+    id: 2,
+    name: "HRMS",
+    industry: "Business",
+    version: "2.1.0",
+    status: "Active",
+  },
+  {
+    id: 3,
+    name: "Inventory",
+    industry: "Retail",
+    version: "1.8.3",
+    status: "Inactive",
+  },
+  {
+    id: 4,
+    name: "Accounting",
+    industry: "Finance",
+    version: "3.0.1",
+    status: "Active",
+  },
+];
+
+export default modules;
