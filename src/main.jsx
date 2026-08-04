0ab8a0743447196f3d608991f426a3efeee4fa0c
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-04 11:33:02 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
-import "./styles/global.css";
+import AppRouter from "./app/router/AppRouter";
 
+import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AppRouter>
+      <App />
+    </AppRouter>
   </React.StrictMode>
 );
