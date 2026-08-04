a441efbe9b1c8c11b0e5e67f0af90911ea48dd06
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-04 16:38:57 +0500

    Create Industries.jsx

---
+++ src/master/pages/Industries/Industries.jsx
@@ -0,0 +1,38 @@
+import MasterLayout from "../../layouts/MasterLayout";
+import "./Industries.css";
+
+import IndustrySearch from "../../components/IndustrySearch/IndustrySearch";
+import IndustryFilters from "../../components/IndustryFilters/IndustryFilters";
+import IndustryTable from "../../components/IndustryTable/IndustryTable";
+
+export default function Industries() {
+  return (
+    <MasterLayout>
+
+      <section className="master-industries">
+
+        <div className="master-page-header">
+
+          <h1>Industries</h1>
+
+          <p>
+            Manage industries and their assigned modules.
+          </p>
+
+        </div>
+
+        <div className="industries-toolbar">
+
+          <IndustrySearch />
+
+          <IndustryFilters />
+
+        </div>
+
+        <IndustryTable />
+
+      </section>
+
+    </MasterLayout>
+  );
+}
