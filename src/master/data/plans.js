7177074867124dcc4763d3a569ac871cd9aa02a8
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-04 16:08:37 +0500

    Create plans.js

---
+++ src/master/data/plans.js
@@ -0,0 +1,40 @@
+const plans = [
+  {
+    id: 1,
+    name: "Starter",
+    price: "$19",
+    billing: "Monthly",
+    companies: 25,
+    users: 100,
+    status: "Active",
+  },
+  {
+    id: 2,
+    name: "Business",
+    price: "$49",
+    billing: "Monthly",
+    companies: 100,
+    users: 500,
+    status: "Active",
+  },
+  {
+    id: 3,
+    name: "Professional",
+    price: "$99",
+    billing: "Monthly",
+    companies: 500,
+    users: 5000,
+    status: "Active",
+  },
+  {
+    id: 4,
+    name: "Enterprise",
+    price: "Custom",
+    billing: "Yearly",
+    companies: "Unlimited",
+    users: "Unlimited",
+    status: "Active",
+  },
+];
+
+export default plans;
