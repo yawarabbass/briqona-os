7621c11b62d301567dcd399f3c470e1ace8408e8
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-04 11:22:26 +0500

    Create masterRoutes.js

---
+++ src/master/routes/masterRoutes.js
@@ -0,0 +1,63 @@
+/*
+|--------------------------------------------------------------------------
+| BRIQONA OS
+|--------------------------------------------------------------------------
+| Master Routes
+|--------------------------------------------------------------------------
+*/
+
+const masterRoutes = [
+
+  {
+    path: "/master/dashboard",
+    page: "Dashboard",
+  },
+
+  {
+    path: "/master/companies",
+    page: "Companies",
+  },
+
+  {
+    path: "/master/plans",
+    page: "Plans",
+  },
+
+  {
+    path: "/master/industries",
+    page: "Industries",
+  },
+
+  {
+    path: "/master/modules",
+    page: "Modules",
+  },
+
+  {
+    path: "/master/users",
+    page: "Users",
+  },
+
+  {
+    path: "/master/billing",
+    page: "Billing",
+  },
+
+  {
+    path: "/master/reports",
+    page: "Reports",
+  },
+
+  {
+    path: "/master/ai",
+    page: "AI Center",
+  },
+
+  {
+    path: "/master/settings",
+    page: "Settings",
+  }
+
+];
+
+export default masterRoutes;
