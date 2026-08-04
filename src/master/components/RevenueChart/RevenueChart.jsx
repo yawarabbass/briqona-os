866f520eebe08701522b2cf7670e70a3f764bf15
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-04 12:23:52 +0500

    Create RevenueChart.jsx

---
+++ src/master/components/RevenueChart/RevenueChart.jsx
@@ -0,0 +1,20 @@
+import "./RevenueChart.css";
+
+export default function RevenueChart() {
+  return (
+    <section className="revenue-chart">
+
+      <div className="revenue-chart-header">
+        <h3>Revenue Overview</h3>
+        <span>Last 12 Months</span>
+      </div>
+
+      <div className="revenue-chart-body">
+        <div className="chart-coming-soon">
+          Revenue Chart
+        </div>
+      </div>
+
+    </section>
+  );
+}
