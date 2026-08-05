c169b85943563c8e2959f83e1da1b9c9b3b3d0f8
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-05 12:49:09 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,12 @@ import Dashboard from "./pages/Dashboard/Dashboard";
 import PublicLayout from "./public/layouts/PublicLayout";
 import PublicRoutes from "./app/router/PublicRoutes";
 import Business from "./pages/Business/Business";
+import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
+
+
+
+
+
 function App() {
 const path = window.location.pathname;
 
@@ -37,7 +43,18 @@ const path = window.location.pathname;
     </div>
   );
 }
+  
+if (path === "/forgot-password") {
+  return (
+    <div className="app">
+      <Header />
+      <ForgotPassword />
+      <Footer />
+    </div>
+  );
+}
 
+  
 if (path === "/demo") {
   return (
     <div className="app">
