cad48f1ecb0331898779c4dbf409800ff49c5df6
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-06 08:29:09 +0500

    Update AppRouter.jsx

--- src/app/router/AppRouter.jsx
+++ src/app/router/AppRouter.jsx
@@ -1,17 +1,24 @@
-/*
-|--------------------------------------------------------------------------
-| BRIQONA OS
-|--------------------------------------------------------------------------
-| Application Router
-|--------------------------------------------------------------------------
-*/
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { BrowserRouter } from "react-router-dom";
+import PublicRoutes from "./PublicRoutes";
+import MasterRoutes from "./MasterRoutes";
+import WorkspaceRoutes from "./WorkspaceRoutes";
 
-export default function AppRouter({ children }) {
+export default function AppRouter() {
   return (
     <BrowserRouter>
-      {children}
+      <Routes>
+
+        {/* Public Website */}
+        <Route path="/*" element={<PublicRoutes />} />
+
+        {/* Master Dashboard */}
+        <Route path="/master/*" element={<MasterRoutes />} />
+
+        {/* Workspace Dashboard */}
+        <Route path="/workspace/*" element={<WorkspaceRoutes />} />
+
+      </Routes>
     </BrowserRouter>
   );
-}
+        }
