e21718c09097210f2cd5fa61b14090af44355cb6
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-06 15:33:07 +0500

    Update Button.jsx

--- src/shared/components/Button/Button.jsx
+++ src/shared/components/Button/Button.jsx
@@ -1,21 +1,62 @@
 import "./Button.css";
 
 export default function Button({
-  children,
-  type = "button",
-  variant = "primary",
-  size = "md",
-  disabled = false,
-  onClick,
-}) {
-  return (
-    <button
-      type={type}
-      disabled={disabled}
-      onClick={onClick}
-      className={`btn btn-${variant} btn-${size}`}
-    >
-      {children}
-    </button>
-  );
+
+children,
+
+type="button",
+
+variant="primary",
+
+size="md",
+
+fullWidth=false,
+
+disabled=false,
+
+icon=null,
+
+onClick,
+
+}){
+
+return(
+
+<button
+
+type={type}
+
+disabled={disabled}
+
+onClick={onClick}
+
+className={`
+btn
+btn-${variant}
+btn-${size}
+${fullWidth ? "btn-block" : ""}
+`}
+
+>
+
+{icon &&
+
+<span className="btn-icon">
+
+{icon}
+
+</span>
+
+}
+
+<span>
+
+{children}
+
+</span>
+
+</button>
+
+);
+
 }
