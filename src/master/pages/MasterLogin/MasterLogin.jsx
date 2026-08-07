c603a900a124dfba828c4fd381c99e9920f4df58
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-07 17:27:14 +0500

    Update MasterLogin.jsx

--- src/master/pages/MasterLogin/MasterLogin.jsx
+++ src/master/pages/MasterLogin/MasterLogin.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./MasterLogin.css";
-
+ 
 export default function MasterLogin() {
   const navigate = useNavigate();
 
