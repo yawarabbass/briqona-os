ca68ee0921350077367173bd8a3689f6910c21dc
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-07 17:07:48 +0500

    Update MasterRoutes.jsx

--- src/app/router/MasterRoutes.jsx
+++ src/app/router/MasterRoutes.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-
+ 
 import MasterLayout from "../../master/layouts/MasterLayout";
 
 import MasterLogin from "../../master/pages/MasterLogin/MasterLogin";
