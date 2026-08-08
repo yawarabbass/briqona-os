7b0901a383f353219e396fb88ff4fd59e8bf0954
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-08 11:53:34 +0500

    Update PlanSearch.jsx

--- src/master/components/PlanSearch/PlanSearch.jsx
+++ src/master/components/PlanSearch/PlanSearch.jsx
@@ -2,18 +2,20 @@ import "./PlanSearch.css";
 
 export default function PlanSearch() {
   return (
-    <section className="plan-search">
-
+    <div className="plan-search">
       <input
         type="text"
         className="plan-search-input"
         placeholder="Search plans..."
+        aria-label="Search plans"
       />
 
-      <button className="plan-search-button">
+      <button
+        type="button"
+        className="plan-search-button"
+      >
         Search
       </button>
-
-    </section>
+    </div>
   );
 }
