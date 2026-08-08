0548ecd593b3b3b41c2d6ec4ff8e0b37ff18603a
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-08 13:09:52 +0500

    Add files via upload

--- src/master/layouts/MasterLayout.jsx
+++ src/master/layouts/MasterLayout.jsx
@@ -1,4 +1,14 @@
-cat > src/master/layouts/MasterLayout.jsx <<'EOF'
+/*
+|--------------------------------------------------------------------------
+| BRIQONA OS
+|--------------------------------------------------------------------------
+| Master Layout
+|--------------------------------------------------------------------------
+|
+| Enterprise SaaS Layout
+|
+*/
+
 import { useState } from "react";
 
 import Header from "../components/Header/Header";
@@ -6,45 +16,67 @@ import Sidebar from "../components/Sidebar/Sidebar";
 
 import "../styles/master.css";
 
-export default function MasterLayout({ children }) {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+export default function MasterLayout({
 
-  const openSidebar = () => {
-    setSidebarOpen(true);
-  };
+children,
 
-  const closeSidebar = () => {
-    setSidebarOpen(false);
-  };
+}){
 
-  return (
-    <div className="master-layout">
+const [sidebarOpen,setSidebarOpen]=useState(false);
 
-      <Sidebar
-        sidebarOpen={sidebarOpen}
-        closeSidebar={closeSidebar}
-      />
+const openSidebar=()=>{
 
-      <div
-        className={`master-overlay ${
-          sidebarOpen ? "show" : ""
-        }`}
-        onClick={closeSidebar}
-      />
+setSidebarOpen(true);
 
-      <div className="master-main">
+};
 
-        <Header
-          openSidebar={openSidebar}
-        />
+const closeSidebar=()=>{
 
-        <main className="master-content">
-          {children}
-        </main>
+setSidebarOpen(false);
 
-      </div>
+};
 
-    </div>
-  );
+return(
+
+<div className="master-layout">
+
+<Sidebar
+
+sidebarOpen={sidebarOpen}
+
+closeSidebar={closeSidebar}
+
+/>
+
+<div
+
+className={`
+master-overlay
+${sidebarOpen ? "show" : ""}
+`}
+
+onClick={closeSidebar}
+
+/>
+
+<div className="master-main">
+
+<Header
+
+openSidebar={openSidebar}
+
+/>
+
+<main className="master-content">
+
+{children}
+
+</main>
+
+</div>
+
+</div>
+
+);
+
 }
-EOF
