da1c45760bfc7e1551184fef39c6c8c44c4b9f77
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-08 11:56:34 +0500

    Update PlanFilters.jsx

--- src/master/components/PlanFilters/PlanFilters.jsx
+++ src/master/components/PlanFilters/PlanFilters.jsx
@@ -2,32 +2,38 @@ import "./PlanFilters.css";
 
 export default function PlanFilters() {
   return (
-    <section className="plan-filters">
+    <div className="plan-filters">
 
       <div className="plan-filter-group">
+        <label htmlFor="plan-billing-filter">
+          Billing
+        </label>
 
-        <label>Billing</label>
-
-        <select>
-          <option>All</option>
-          <option>Monthly</option>
-          <option>Yearly</option>
+        <select
+          id="plan-billing-filter"
+          defaultValue="All"
+        >
+          <option value="All">All</option>
+          <option value="Monthly">Monthly</option>
+          <option value="Yearly">Yearly</option>
         </select>
-
       </div>
 
       <div className="plan-filter-group">
+        <label htmlFor="plan-status-filter">
+          Status
+        </label>
 
-        <label>Status</label>
-
-        <select>
-          <option>All</option>
-          <option>Active</option>
-          <option>Inactive</option>
+        <select
+          id="plan-status-filter"
+          defaultValue="All"
+        >
+          <option value="All">All</option>
+          <option value="Active">Active</option>
+          <option value="Inactive">Inactive</option>
         </select>
-
       </div>
 
-    </section>
+    </div>
   );
 }
