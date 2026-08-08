aa925ebdcc54e72eaf77dc895bcc4ab3b0bf4366
Yawar Abbas Bhatti <35185083+yawarabbass@users.noreply.github.com>
2026-08-08 15:12:41 +0500

    Update Plans.jsx

--- src/master/pages/Plans/Plans.jsx
+++ src/master/pages/Plans/Plans.jsx
@@ -1,4 +1,3 @@
-
 import "./Plans.css";
 
 import PlanSearch from "../../components/PlanSearch/PlanSearch";
@@ -7,31 +6,52 @@ import PlanTable from "../../components/PlanTable/PlanTable";
 
 export default function Plans() {
   return (
-  
+    <section className="master-plans">
 
-      <section className="master-plans">
+      {/* PAGE HEADER */}
+      <div className="master-page-header">
 
-        <div className="master-page-header">
+        <div className="master-page-heading">
 
-          <h1>Plans</h1>
+          <span className="master-page-label">
+            SUBSCRIPTION MANAGEMENT
+          </span>
+
+          <h1>
+            Plans
+          </h1>
 
           <p>
-            Manage subscription plans and pricing.
+            Manage subscription plans, pricing,
+            billing cycles and workspace limits.
           </p>
 
         </div>
 
-        <div className="plans-toolbar">
+      </div>
 
+
+      {/* TOOLBAR */}
+      <div className="plans-toolbar">
+
+        <div className="plans-search-section">
           <PlanSearch />
-
-          <PlanFilters />
-
         </div>
+
+        <div className="plans-filter-section">
+          <PlanFilters />
+        </div>
+
+      </div>
+
+
+      {/* PLAN TABLE */}
+      <div className="plans-content">
 
         <PlanTable />
 
-      </section>
+      </div>
 
+    </section>
   );
 }
